160fc844957325604ad53e24d00f3159f9cf6afd
Ved Pandey <vpdevs001@gmail.com>
2026-05-03 08:07:20 +0530

    Rotate Keys + Fix Wrong Path name

---
+++ key-gen.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo Creating cert folder...
+if not exist cert (
+    mkdir cert
+)
+
+cd cert
+
+echo.
+
+if exist private.pem (
+    echo private.pem already exists. Skipping...
+) else (
+    echo Generating private key...
+    openssl genrsa -out private.pem 2048
+)
+
+echo.
+
+if exist public.pem (
+    echo public.pem already exists. Skipping...
+) else (
+    echo Generating public key...
+    openssl rsa -in private.pem -pubout -out public.pem
+)
+
+echo.
+echo Done!
+echo Keys location: %cd%
+pause
